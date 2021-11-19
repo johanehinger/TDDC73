828bcd543c223f13446841469c5296fc48133951
Johan Ehinger <johan.ehinger@hotmail.com>
2021-11-19 10:52:43 +0100

    graphgl: Add mock graphQL.

--- lab3/lib/main.dart
+++ lab3/lib/main.dart
@@ -1,7 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+
+const testGraphQL = """
+query test {
+  search(query: "language:Dart stars:30..40", type: REPOSITORY, first: 10) {
+    edges {
+      node {
+        ... on Repository {
+          name
+        }
+      }
+    }
+  }
+}
+""";
 
 void main() {
-  runApp(const MyApp());
+  final HttpLink httpLink = HttpLink("https://api.github.com/graphql");
+
+  final AuthLink authLink = AuthLink(
+    getToken: () async => 'Bearer <mock_token>',
+  );
+
+  final Link link = authLink.concat(httpLink);
+
+  ValueNotifier<GraphQLClient> client = ValueNotifier(
+    GraphQLClient(
+      link: link,
+      cache: GraphQLCache(
+        store: InMemoryStore(),
+      ),
+    ),
+  );
+  var app = GraphQLProvider(
+    client: client,
+    child: const MyApp(),
+  );
+
+  runApp(app);
 }
 
 class MyApp extends StatelessWidget {
@@ -14,7 +50,41 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Scaffold(),
+      home: Scaffold(
+        appBar: AppBar(),
+        body: Query(
+          options: QueryOptions(document: gql(testGraphQL)),
+          builder: (QueryResult result, {fetchMore, refetch}) {
+            if (result.hasException) {
+              debugPrint(result.exception.toString());
+              return Text(result.exception.toString());
+            }
+            if (result.isLoading) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            var repoList = result.data?['search']['edges'];
+            return Column(
+              children: [
+                const Text("Repos"),
+                Expanded(
+                  child: GridView.builder(
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2),
+                    itemCount: repoList.length,
+                    itemBuilder: (_, index) {
+                      var repo = repoList[index]['node'];
+                      return Text(repo["name"]);
+                    },
+                  ),
+                )
+              ],
+            );
+          },
+        ),
+      ),
     );
   }
 }
